93f3245d188be9ed44268aaa004e4ed814e8cdce
Jostyn G <Jostyn G@Jostyn.mshome.net>
2015-11-28 18:43:53 -0500

    variable por valor

---
+++ src/ejercicio_4/ejercicio_4.java
@@ -0,0 +1,16 @@
+package ejercicio_4;
+
+public class ejercicio_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=12;
+int j=17;
+int z=20;
+	i=100;
+	System.out.println("i:" +i);
+	System.out.println("j:" +j);
+	System.out.println("z" +z);
+	}
+
+}
